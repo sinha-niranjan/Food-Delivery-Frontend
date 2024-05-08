64df6856ccf187c03d0f742549190a72250e5d6f
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-05-08 22:50:32 +0530

    frontend initalized

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      Click me {count}
+    </Button>
+  );
+}
+
+export default App;
