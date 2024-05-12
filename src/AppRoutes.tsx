4fd8da0b687742e0ec9bb120eae0e09063d47b2c
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-05-12 17:51:05 +0530

    protected Route is added

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/layout";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import Home from "./pages/Home";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
@@ -10,20 +11,24 @@ const AppRoutes = () => {
       <Route
         path="/"
         element={
-          <Layout showHero={true} >
-            <Home   />
+          <Layout showHero={true}>
+            <Home />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route
-        path="/user-profile"
-        element={
-          <Layout>
-            <UserProfilePage />
-          </Layout>
-        }
-      />
+
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
+
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
