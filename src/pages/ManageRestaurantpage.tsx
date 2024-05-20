3d134db667bb174b013557eb7448791bbe7ff424
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-05-21 05:20:14 +0530

    create restaurant added

--- src/pages/ManageRestaurantpage.tsx
+++ src/pages/ManageRestaurantpage.tsx
@@ -1,7 +1,11 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantpage = () => {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
 };
 
 export default ManageRestaurantpage;
--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,26 @@ export type User = {
     country: string;
   };
 };
+
+export type MenuItem = {
+  _id: string;
+  name: string;
+  price: number;
+};
+
+export type Restaurant = {
+  message: string;
+  restaurant: {
+    _id: string;
+    user: string;
+    restaurantName: string;
+    city: string;
+    country: string;
+    deliveryPrice: number;
+    estimatedDeliveryTime: number;
+    cuisines: string[];
+    menuItems: MenuItem[];
+    imageUrl: string;
+    lastUpdated: string;
+  };
+};
